58f145f941490df38300d99665e41bf25548d630
Artur Taboada <arturtaboada@gmail.com>
2026-09-20 18:32:06 -0300

    matriz

--- grafos.h
+++ grafos.h
@@ -4,11 +4,22 @@
 using namespace std;
 
 #include <fstream>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+struct ArvoreBusca{
+    int raiz;
+    vector<int> pai;
+    vector<int> nivel;
+    int nivelMaximo;
+    int maisDistante;
+};
 
 class Grafo{
     public:
         Grafo(int tipoGrafo, ifstream& arquivo); // item 1 e item 3
-        ifstream saidaGrafo(); // item 2, item 6 e item 5 (diametro do grafo)
+        ofstream saidaGrafo(); // item 2, item 6 e item 5 (diametro do grafo)
             //numero de vertices, 
             //numero de arestas, 
             //grau minimo, 
@@ -17,28 +28,34 @@ class Grafo{
             //mediana de grau
             //diametro do grafo,
             //numero de componentes conexas,
-        ifstream bfs(int vertice); // item 4
-        ifstream dfs(int vertice); // item 4
-        ifstream distancia(int vertice1, int vertice2); //item 5 (distancia entre dois vertices)
+        ArvoreBusca bfs(int vertice); // item 4
+        ArvoreBusca dfs(int vertice); // item 4
+//        int distancia(int vertice1, int vertice2); //item 5 (distancia entre dois vertices)
+//        int diametro(); //item 5 (diametro do grafo)
+        vector<vector<int>> componentesConexas(); // item 6
 
     private:
         int tipo; // 0 = lista de adjacencia, 1 = matriz de adjacencia
+        const int numVertices;
+        vector<vector<bool>> matrizAdjacencia;
+        vector<vector<int>> componentesConexasMatriz();
 
         void inicializaLista(ifstream& arquivo);
         void inicializaMatriz(ifstream& arquivo);
 
-        ifstream saidaLista();
-        ifstream saidaMatriz();
+        ofstream saidaLista();
+        ofstream saidaMatriz();
 
-        ifstream bfsLista(int vertice);
-        ifstream bfsMatriz(int vertice);
+        ofstream bfsLista(int vertice);
+        ArvoreBusca bfsMatriz(int vertice);
 
-        ifstream dfsLista(int vertice);
-        ifstream dfsMatriz(int vertice);
+        ofstream dfsLista(int vertice);
+        ArvoreBusca dfsMatriz(int vertice);
 
         
-        ifstream distanciaLista(int vertice1, int vertice2);
-        ifstream distanciaMatriz(int vertice1, int vertice2);
+        int distanciaLista(int vertice1, int vertice2);
+        int distanciaMatriz(int vertice1, int vertice2);
+        int diametroMatriz();
 };
 
 #endif
